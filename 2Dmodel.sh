9ede0dd16fb69bf2630eef8f994b02752abe3111
llien30 <mana.smiles@gmail.com>
2020-10-12 01:16:06 +0900

    del .sh file

--- 2Dmodel.sh
+++
@@ -1,4 +0,0 @@
-env CUDA_VISIBLE_DEVICES=1 python train_F.py ./config/sep_anomal/mana_1.yaml --no_wandb > hoge1.out &
-env CUDA_VISIBLE_DEVICES=1 python train_F.py ./config/sep_anomal/mana_2.yaml --no_wandb > hoge2.out &
-env CUDA_VISIBLE_DEVICES=1 python train_F.py ./config/sep_anomal/mana_3.yaml --no_wandb > hoge3.out &
-env CUDA_VISIBLE_DEVICES=1 python train_F.py ./config/sep_anomal/mana_4.yaml --no_wandb > hoge4.out &
